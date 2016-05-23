cbb70f132a5031409c669c97972eed228be023cb
Rob Head <robert.head@gmail.com>
2016-05-23 16:59:31 -0700

    Refactor with objects.
    [Start #120152031]

--- app/assets/javascripts/game.js
+++ app/assets/javascripts/game.js
@@ -1,34 +1,44 @@
+function Sound(id, uri) {
+  this.register = function(uri) {
+    createjs.Sound.registerSound(uri, this.id);
+  };
+
+  this.play = function() {
+    createjs.Sound.play(this.id);
+  }
+
+  this.id = id;
+  this.register(uri);
+}
+
+function Circle(x, y, radius, color, sound) {
+  this.defineEaselShape = function(x, y, radius, color) {
+    var circle = new createjs.Shape();
+    circle.graphics.beginFill(color).drawCircle(0, 0, radius);
+    circle.x = x;
+    circle.y = y;
+    return circle;
+  }
+
+  this.addClickSound = function(sound) {
+    this.easelShape.addEventListener("click", function(target) {
+      sound.play();
+    });
+  }
+
+  this.easelShape = this.defineEaselShape(x, y, radius, color);
+  this.addClickSound(sound);
+}
+
 var Game = {
   canvasId: "gameCanvas",
-  soundId: "Thunder",
 
   start: function() {
-    this.loadSound();
+    this.sound = new Sound("Thunder", "sounds/thunder.mp3");
+    this.circle = new Circle(100, 100, 50, "DeepSkyBlue");
     this.stage = new createjs.Stage(this.canvasId);
-    this.drawCircle();
+    this.stage.addChild(this.circle.easelShape);
     this.stage.update();
-  },
-
-  drawCircle: function() {
-    var circle = new createjs.Shape();
-    circle.graphics.beginFill("DeepSkyBlue").drawCircle(0, 0, 50);
-    circle.x = 100;
-    circle.y = 100;
-
-    circle.addEventListener("click", function(target) {
-      Game.playSound(Game.soundId);
-    });
-
-    this.stage.addChild(circle);
-  },
-
-  loadSound: function() {
-    createjs.Sound.registerSound("sounds/thunder.mp3", Game.soundId);
-  },
-
-  playSound: function(soundId) {
-    Game.log(soundId);
-    createjs.Sound.play(soundId);
   },
 
   log: function(content) {
