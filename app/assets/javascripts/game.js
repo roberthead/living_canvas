ccd42f89256abadde69a605ce5812fe30cfb7e70
Rob Head <robert.head@gmail.com>
2016-06-07 15:57:00 -0700

    Add Game Loop

--- app/assets/javascripts/game.js
+++ app/assets/javascripts/game.js
@@ -36,12 +36,51 @@ function Circle(x, y, radius, color, sound) {
 var Game = {
   canvasId: "gameCanvas",
   circleCount: 0,
+  startTime: null,
+  lastFrameTime: null,
+  currentFrameTime: null,
+  elapsedFrameTime: 0,
+  elapsedTime: 0,
+  secondsDisplay: 0,
+  computedFPS: 0,
 
   start: function() {
     this.stage = new createjs.Stage(this.canvasId);
     this.addCircle("DeepSkyBlue", "sounds/thunder.mp3");
     this.addCircle("Red", "//s3-us-west-2.amazonaws.com/pagescape/BBCNNBC.mp3");
     this.stage.update();
+    window.requestAnimationFrame(this.executeFrame.bind(this));
+  },
+
+  executeFrame: function() {
+    this.setFrameTimes();
+    this.processActions();
+    this.drawFrame();
+    window.requestAnimationFrame(this.executeFrame.bind(this));
+  },
+
+  setFrameTimes: function() {
+    // all times in milliseconds
+    // all absolute times in milliseconds since Epoch
+    this.startTime = this.startTime || new Date().getTime();
+    this.lastFrameTime = this.currentFrameTime || this.startTime;
+    this.currentFrameTime = new Date().getTime();
+    this.elapsedFrameTime = this.currentFrameTime - this.lastFrameTime;
+    this.elapsedTime = this.currentFrameTime - this.startTime;
+    // average two cycles to get framerate
+    var currentFPS = this.elapsedFrameTime > 0 ? 1000.0 / this.elapsedFrameTime : 60;
+    this.computedFPS = ((this.computedFPS || currentFPS) + currentFPS) / 2;
+  },
+
+  processActions: function() {
+    var elapsedSeconds = Math.floor(this.elapsedTime / 1000);
+    if (this.secondsDisplay != elapsedSeconds) {
+      this.secondsDisplay = elapsedSeconds;
+      console.log("" + this.secondsDisplay + ": " + this.computedFPS + "fps")
+    }
+  },
+
+  drawFrame: function() {
   },
 
   addCircle: function(color, soundUri) {
